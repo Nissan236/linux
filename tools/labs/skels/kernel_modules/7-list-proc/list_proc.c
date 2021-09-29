eea9e1647d0500e494148bd817afcb3a89b18afc
Nissan236 <nissan5931@gmail.com>
2021-09-29 12:37:27 +0300

    added solution for task 7

--- tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
+++ tools/labs/skels/kernel_modules/7-list-proc/list_proc.c
@@ -1,7 +1,8 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-/* TODO: add missing headers */
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 MODULE_DESCRIPTION("List current processes");
 MODULE_AUTHOR("Kernel Hacker");
@@ -11,16 +12,21 @@ static int my_proc_init(void)
 {
 	struct task_struct *p;
 
-	/* TODO: print current process pid and its name */
+	pr_info("Current process:\n");
+	pr_info("pid: %d\nexecutable name: %s\n", current->pid, current->comm);
 
-	/* TODO: print the pid and name of all processes */
+	pr_info("All processes:\n");
+	for_each_process(p)
+	{
+		pr_info("pid: %d\nexecutable name: %s\n", p->pid, p->comm);
+	}
 
 	return 0;
 }
 
 static void my_proc_exit(void)
 {
-	/* TODO: print current process pid and name */
+	pr_info("pid: %d\nexecutable name: %s\n", current->pid, current->comm);
 }
 
 module_init(my_proc_init);
